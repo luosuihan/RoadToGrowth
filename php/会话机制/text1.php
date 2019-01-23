00125e1fb40543d0b7e8f66455f7b7f835139bbd
luosuihan <m18598771456@163.com>
2019-01-23 14:38:48 +0800

    前端界面

--- php/会话机制/text1.php
+++ php/会话机制/text1.php
@@ -5,3 +5,6 @@
  * Date: 2019/1/22
  * Time: 14:12
  */
+session_start();
+$_SESSION['name'] = 'amao';
+$_SESSION['age'] = 3;
--- php/会话机制/text2.php
+++ php/会话机制/text2.php
@@ -5,3 +5,5 @@
  * Date: 2019/1/22
  * Time: 14:13
  */
+session_start();
+var_dump($_SESSION);
